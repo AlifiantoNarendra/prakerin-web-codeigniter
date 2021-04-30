246af2dcf0d8dc2a76124e2b1938209c3c2b22f7
AlifiantoNarendra <alifiantonarendra@gmail.com>
2021-04-30 08:00:42 +0700

    Install Codeigniter dan Membuat front End

---
+++ app/Views/index.php
@@ -0,0 +1,110 @@
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-dark shadow-sm fixed-top bg-primary">
+            <div class="container">
+                <a class="navbar-brand" href="#">Alifianto Narendra</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#home">Home</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- jumbotron -->
+        <section class="jumbotron text-center mt-5">
+            <img src="/assets/foto.jpg" alt="My Photo" width="200" class="rounded-circle img-thumbnail" />
+            <h1 class="display-4">Alifianto Narendra</h1>
+            <p class="lead">Wellcome to My Portfolio</p>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path
+                fill="#ffffff"
+                fill-opacity="1"
+                d="M0,128L48,117.3C96,107,192,85,288,96C384,107,480,149,576,165.3C672,181,768,171,864,186.7C960,203,1056,245,1152,250.7C1248,256,1344,224,1392,208L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+                ></path>
+            </svg>
+        </section>
+
+        <!-- about -->
+    <section class="about" id="about">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col">
+            <h2 class="mb-4">About Me</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center fs-5 text-center">
+          <div class="col-md-4">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio sunt neque doloribus deserunt libero voluptates cum amet veritatis similique accusamus?</p>
+          </div>
+          <div class="col-md-4">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis officia odit veritatis aut, commodi illum aspernatur alias dolorum sapiente omnis saepe magnam autem sit quo.</p>
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#7fd0d6"
+          fill-opacity="1"
+          d="M0,64L24,85.3C48,107,96,149,144,144C192,139,240,85,288,80C336,75,384,117,432,133.3C480,149,528,139,576,154.7C624,171,672,213,720,202.7C768,192,816,128,864,128C912,128,960,192,1008,213.3C1056,235,1104,213,1152,181.3C1200,149,1248,107,1296,96C1344,85,1392,107,1416,117.3L1440,128L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"
+        ></path>
+      </svg>
+    </section>
+
+
+    <!-- contact -->
+    <section id="contact" class="text-white" style="background-color: #7fd0d6;">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col">
+            <h2>Contact Me</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-6">
+            <form>
+              <div class="mb-3">
+                <label for="name" class="form-label">Nama Lengkap</label>
+                <input type="text" class="form-control" id="name" placeholder="Name" />
+              </div>
+              <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="email" placeholder="name@example.com" />
+              </div>
+              <div class="mb-3">
+                <label for="pesan" class="form-label">Masukkan Pesan Anda</label>
+                <textarea class="form-control" id="pesan" rows="3"></textarea>
+              </div>
+              <button type="submit" class="btn ps-4 pe-4 text-white bg-primary">Kirim</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#ffff"
+          fill-opacity="1"
+          d="M0,192L34.3,186.7C68.6,181,137,171,206,176C274.3,181,343,203,411,218.7C480,235,549,245,617,218.7C685.7,192,754,128,823,122.7C891.4,117,960,171,1029,181.3C1097.1,192,1166,160,1234,149.3C1302.9,139,1371,149,1406,154.7L1440,160L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"
+        ></path>
+      </svg>
+    </section>
+
+
+    <!-- footer -->
+    <footer style="background-color: white;" class=" text-center pb-4">
+      <p>Createt with <i class="bi bi-heart-fill text-danger"></i> by <a href="#" class=" fw-bold">Alifianto Narendra</a></p>
+    </footer>
+
+
+
+    
---
+++ app/Views/layout/header.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+        <link href="<?= base_url('https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css') ?>" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+        <!-- Bootsrtrap Icon -->
+        <link rel="stylesheet" href="<?= base_url('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css') ?>" />
+
+        <style>
+            section {
+                padding-top: 5rem;
+            }
+            .jumbotron {
+                background-color: #7fd0d6;
+                color: white;
+            }
+            .jumbotron .display-4 {
+                font-weight: bold;
+                text-shadow: 2px 2px 2px black;
+            }
+            .jumbotron .lead {
+                font-weight: bold;
+            }
+        </style>
+
+        <title>Front End</title>
+    </head>
+
+    <body>
